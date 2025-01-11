282d2e82184c815cc6c4a8721941bc7c064dd511
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-11 12:21:09 -0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from "./routes/Home.tsx";
+import ErrorPage from './routes/ErrorPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
